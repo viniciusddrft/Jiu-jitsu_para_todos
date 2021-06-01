a86798f1bb956668f0bd34ce00b2931638047e05
viniciusddrft <viniciusddrft5@gmail.com>
2021-06-01 16:16:50 -0300

    Correction of unit tests

--- test/controller/controller_quiz_test.dart
+++ test/controller/controller_quiz_test.dart
@@ -1,6 +1,5 @@
 import 'package:jiu_jitsu_para_todos/src/controllers/quiz/quiz_controller.dart';
 import 'package:flutter_test/flutter_test.dart';
-//import 'package:jiu_jitsu_para_todos/src/controllers/sound/controller_sound.dart';
 
 main() {
   /*
@@ -9,13 +8,12 @@ main() {
     this test instantiates 2 controllers and tests whether they return all questions at random.
     they need to be the same size to be validated.
   */
+  int numberOfDifferentQuestions = 0;
   bool isQuestionsDifferents = false;
   ControllerQuiz _controllerquiz1 = ControllerQuiz();
   var myquestions1 = _controllerquiz1.choice('easy');
   ControllerQuiz _controllerquiz2 = ControllerQuiz();
   var myquestions2 = _controllerquiz2.choice('easy');
-
-  //MySound mysound = MySound();
 
   test('deve retornar todas questões de forma aleatoria', () {
     List<String> _arrayTeste1 = [];
@@ -30,13 +28,18 @@ main() {
     }
 
     if (_arrayTeste1.length == _arrayTeste2.length) {
-      if (_arrayTeste1 != _arrayTeste2) {
-        isQuestionsDifferents = true;
-        expect(isQuestionsDifferents, true);
-      } else {
-        expect(isQuestionsDifferents,
-            true); //error as questions returned are equal
+      for (var _i = 0; _i != _arrayTeste1.length; _i++) {
+        if (_arrayTeste1[_i] != _arrayTeste2[_i]) {
+          numberOfDifferentQuestions++;
+          isQuestionsDifferents = true;
+        }
       }
+      print('Of ' +
+          _arrayTeste1.length.toString() +
+          ' questions ' +
+          numberOfDifferentQuestions.toString() +
+          ' were in different order');
+      expect(isQuestionsDifferents, true);
     } else {
       expect(isQuestionsDifferents,
           true); //purposeful error if it returns different numbers of questions
