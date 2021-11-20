35c01404e083766549593327d758621f1e83b16d
viniciusddrft <viniciusddrft5@gmail.com>
2021-11-19 21:22:16 -0300

    update

--- lib/core/routes/routes_app.dart
+++ lib/core/routes/routes_app.dart
@@ -19,94 +19,95 @@ import 'package:jiu_jitsu_para_todos/src/modules/wallpapers/view/wallpapersmenu/
 
 class Routes {
   static Route<dynamic>? routes(RouteSettings settings) {
-    if (settings.name == "/Home") {
-      return MaterialPageRoute(
-        builder: (BuildContext context) => const HomePageView(),
-      );
-    } else if (settings.name == "/Settings") {
-      return TransitionElasticOut(
-        duration: const Duration(milliseconds: 500),
-        route: const SettingsView(),
-      );
-    } else if (settings.name == "/Credits") {
-      return TransitionLeftToRight(
-        duration: const Duration(milliseconds: 300),
-        route: const CreditsView(),
-      );
-    } else if (settings.name == "/HistoryOfJiujitsu") {
-      return TransitionElasticOut(
-        duration: const Duration(milliseconds: 500),
-        route: const HistoryOfJiuJitsuView(),
-      );
-    } else if (settings.name == "/Rules") {
-      return TransitionElasticOut(
-        duration: const Duration(milliseconds: 500),
-        route: const RulesView(),
-      );
-    } else if (settings.name == "/Quiz") {
-      return TransitionElasticOut(
-        duration: const Duration(milliseconds: 500),
-        route: const QuizView(),
-      );
-    } else if (settings.name == "/Fightmarker") {
-      return TransitionElasticOut(
-        duration: const Duration(milliseconds: 500),
-        route: const FightMakerView(),
-      );
-    } else if (settings.name == "/Wallpapers") {
-      return TransitionElasticOut(
-        duration: const Duration(milliseconds: 500),
-        route: const WallpapersView(),
-      );
-    } else if (settings.name == "/JiujitsuInBrazil") {
-      return TransitionLeftToRight(
-        duration: const Duration(milliseconds: 300),
-        route: const JiujitsuinBrazil(),
-      );
-    } else if (settings.name == "/OriginOfJiujitsu") {
-      return TransitionLeftToRight(
-        duration: const Duration(milliseconds: 300),
-        route: const OriginOfJiujitsu(),
-      );
-    } else if (settings.name == "/BasicRules") {
-      return TransitionLeftToRight(
-        duration: const Duration(milliseconds: 300),
-        route: const Basicrules(),
-      );
-    } else if (settings.name == "/CbjjRules") {
-      return TransitionLeftToRight(
-        duration: const Duration(milliseconds: 300),
-        route: const Cbjjrules(),
-      );
-    } else if (settings.name == "/DetailsImage" && settings.arguments != null) {
-      return MaterialPageRoute(
-        builder: (BuildContext context) => DetailsImage(
-          imagePath: (settings.arguments as Map<String, dynamic>)['imagePath'],
-          index: (settings.arguments as Map<String, dynamic>)['index'],
-        ),
-      );
-    } else if (settings.name == "/QuizQuestions" &&
-        settings.arguments != null) {
-      return TransitionLeftToRight(
-        duration: const Duration(milliseconds: 300),
-        route: QuizQuestions(
-          difficulty:
-              (settings.arguments as Map<String, String>)['difficulty']!,
-          difficultyName:
-              (settings.arguments as Map<String, String>)['difficultyName']!,
-        ),
-      );
-    } else if (settings.name == "/ResultQuiz" && settings.arguments != null) {
-      return TransitionElasticOut(
-        duration: const Duration(milliseconds: 500),
-        route: ResultQuiz(
-          difficultyName:
-              (settings.arguments as Map<String, dynamic>)['difficultyName'],
-          score: (settings.arguments as Map<String, dynamic>)['score'],
-          totalQuestions:
-              (settings.arguments as Map<String, dynamic>)['totalQuestions'],
-        ),
-      );
+    switch (settings.name) {
+      case '/Home':
+        return MaterialPageRoute(
+          builder: (BuildContext context) => const HomePageView(),
+        );
+      case '/Settings':
+        return TransitionElasticOut(
+          duration: const Duration(milliseconds: 500),
+          route: const SettingsView(),
+        );
+      case '/Credits':
+        return TransitionLeftToRight(
+          duration: const Duration(milliseconds: 300),
+          route: const CreditsView(),
+        );
+      case '/HistoryOfJiujitsu':
+        return TransitionElasticOut(
+          duration: const Duration(milliseconds: 500),
+          route: const HistoryOfJiuJitsuView(),
+        );
+      case '/Rules':
+        return TransitionElasticOut(
+          duration: const Duration(milliseconds: 500),
+          route: const RulesView(),
+        );
+      case '/Quiz':
+        return TransitionElasticOut(
+          duration: const Duration(milliseconds: 500),
+          route: const QuizView(),
+        );
+      case '/Fightmarker':
+        return TransitionElasticOut(
+          duration: const Duration(milliseconds: 500),
+          route: const FightMakerView(),
+        );
+      case '/Wallpapers':
+        return TransitionElasticOut(
+          duration: const Duration(milliseconds: 500),
+          route: const WallpapersView(),
+        );
+      case '/JiujitsuInBrazil':
+        return TransitionLeftToRight(
+          duration: const Duration(milliseconds: 300),
+          route: const JiujitsuinBrazil(),
+        );
+      case '/OriginOfJiujitsu':
+        return TransitionLeftToRight(
+          duration: const Duration(milliseconds: 300),
+          route: const OriginOfJiujitsu(),
+        );
+      case '/BasicRules':
+        return TransitionLeftToRight(
+          duration: const Duration(milliseconds: 300),
+          route: const Basicrules(),
+        );
+      case '/CbjjRules':
+        return TransitionLeftToRight(
+          duration: const Duration(milliseconds: 300),
+          route: const Cbjjrules(),
+        );
+      case '/DetailsImage':
+        return MaterialPageRoute(
+          builder: (BuildContext context) => DetailsImage(
+            imagePath:
+                (settings.arguments as Map<String, dynamic>)['imagePath'],
+            index: (settings.arguments as Map<String, dynamic>)['index'],
+          ),
+        );
+      case '/QuizQuestions':
+        return TransitionLeftToRight(
+          duration: const Duration(milliseconds: 300),
+          route: QuizQuestions(
+            difficulty:
+                (settings.arguments as Map<String, String>)['difficulty']!,
+            difficultyName:
+                (settings.arguments as Map<String, String>)['difficultyName']!,
+          ),
+        );
+      case '/ResultQuiz':
+        return TransitionElasticOut(
+          duration: const Duration(milliseconds: 500),
+          route: ResultQuiz(
+            difficultyName:
+                (settings.arguments as Map<String, dynamic>)['difficultyName'],
+            score: (settings.arguments as Map<String, dynamic>)['score'],
+            totalQuestions:
+                (settings.arguments as Map<String, dynamic>)['totalQuestions'],
+          ),
+        );
     }
   }
 }
